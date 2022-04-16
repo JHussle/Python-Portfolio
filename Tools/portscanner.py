6f51ce1db68379cb03a906133c10604ea626526d
JSmith <65369149+JHussle@users.noreply.github.com>
2022-04-15 19:07:58 -0500

    updated port scanner to handle threading. will only check for open ports.

--- Tools/portscanner.py
+++ Tools/portscanner.py
@@ -1,7 +1,13 @@
 import socket
+from queue import Queue
+import threading
 
-target = "192.168.1.252"
+target = "XXX.XXX.XXX.XXX" # Target port that needs to be scanned
 
+q = Queue()
+for x in range(1, 501):
+    q.put(x)
+    
 def portscan(port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,8 +15,15 @@ def portscan(port):
         return True
     except:
         return False
-for x in range(1, 501):
-    if(portscan(x)):
-        print("Port {} is open!".format(x))
-    else:
-        print("Port {} is closed!".format(x))
+def worker():
+    while True:
+        port = q.get()
+        if portscan(port):
+            print("Port {} is open!")
+
+        else:
+            print("Port {} is closed!".format(x))
+
+for x in range(30):
+    t = threading.Thread(target=worker)
+    t.start()
