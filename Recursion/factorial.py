a335851a6cf707125ec1ad3445f1245006fe5ce7
JSmith <65369149+JHussle@users.noreply.github.com>
2022-04-17 18:44:23 -0500

    New file on recurison and a Database file

---
+++ Recursion/factorial.py
@@ -0,0 +1,6 @@
+def factorial(n):
+    if n < 1:
+        return 1
+    else:
+        number = n * factorial(n-1)
+        return number
---
+++ Recursion/playground.py
@@ -0,0 +1,3 @@
+def function():
+    function()
+function()
