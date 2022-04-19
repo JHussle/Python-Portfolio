f344e8a5847c12aff995b28498d39e07f728328f
JSmith <65369149+JHussle@users.noreply.github.com>
2022-04-19 13:58:53 -0500

    new choas.py file

--- Python Programming/choas.py
+++ Python Programming/choas.py
@@ -2,6 +2,10 @@ def main():
     print("This program illustrates a chaotic funcation")
     x = eval(input("Enter a number between 0 and 1:"))
     for i in range(10):
-        x = 3.9 * x * (1 - x)
+        x = 2.0 * x * (1 - x)
         print(x)
+    y = eval(input("Enter a number between 0 and 1:"))
+    for i in range(10):
+        y = 2.0 * y * (1 - x)
+        print(y)   
 main()
