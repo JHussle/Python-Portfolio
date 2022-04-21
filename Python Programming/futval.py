699be21b2d6e9c79a8b55acebb964fc1cfcd1151
JSmith <65369149+JHussle@users.noreply.github.com>
2022-04-21 18:26:28 -0500

    updated document

--- Python Programming/futval.py
+++ Python Programming/futval.py
@@ -1,6 +1,45 @@
 # futval.py
 #   A program to compute the value of an investment
-#   carried 10 years into the future
+#   carried XX years into the future
+
+############################################################################
+#
+# Print an introduction
+# Get value of principal and apr from user
+# Create a 320x240 GraphWin Titled "Investment Growth Chart"
+#   -- Draw label " 0.0k" at (20,230)
+#   -- Draw label " 2.5k" at (20,180) 
+#   -- Draw label " 5.0k" at (20,130)
+#   -- Draw label " 7.5k" at (20,80)
+#   -- Draw label " 10.0k" at (20,30)
+#  Draw a rectangle from (40, 230) to (65,230 - principal * 0.02)
+# 
+# Create a GraphWin
+# Draw bar at the position 0 with height corresponding to principal
+# For successive yearr 1 through 10 years
+#       -- Calculate principal = principal * (1 + apr)
+#       -- Draw a bar for this year having height corresponding to principal
+# 
+# Wait for user to press enter
+#
+########################################################################
+# 
+# For year running form a value of 1 up through 10:
+#   -- Calculate principal = principal * (1 + apr)
+#   -- Calculate x11 = 25 * year + 40
+#   -- Calclate height = principal * 0.02
+#   -- Draw a rectangle from (x11, 230) to (x11+25, 230 - height)
+# 
+###########################################################################
+#
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+###########################################################################
 
 def main():
     print("This program calculate the future value")
